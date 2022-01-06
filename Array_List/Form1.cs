fac6e1544d1e7e6be8ee6f50e5d819b597133538
mehmettalii <84227393+mehmettalii@users.noreply.github.com>
2022-01-06 21:14:01 +0300

    ArrayList

---
+++ Array_List/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList iller = new ArrayList();
+
+        bool guncellemeMi = false;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string il = textBox1.Text;
+            //if (iller.Contains(il) == true)
+            //{
+            //    MessageBox.Show("Bu il zaten ekli");
+            //    textBox1.Clear();
+            //}
+            //else
+            //{
+            //    iller.Add(il);
+
+            //    //DataSource nin alternatifi
+            //    listBox1.Items.Clear();
+
+            //    foreach (var item in iller)
+            //    {
+            //        listBox1.Items.Add(item);
+            //    }
+
+            //    //listBox1.DataSource = null;
+            //    //listBox1.DataSource = iller;
+            //    textBox1.Clear();
+            //    textBox1.Focus();
+            //}
+
+            string il = textBox1.Text;
+            if (guncellemeMi == false)
+            {
+                iller.Add(il);
+            }
+            else
+            {
+                int selectedIndex = listBox1.SelectedIndex;
+                iller[selectedIndex] = il;
+                button1.Text = "Ekle";
+                guncellemeMi = false;
+            }
+
+            listBox1.Items.Clear();
+            foreach (var item in iller)
+            {
+                listBox1.Items.Add(item);
+            }
+
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = listBox1.SelectedItem.ToString();
+            //iller.Remove(listBox1.SelectedItem);
+            //listBox1.DataSource = null;
+            //listBox1.DataSource = iller;
+            int selectedIndex = listBox1.SelectedIndex;
+            string seciliIl = iller[selectedIndex].ToString();
+            textBox1.Text = seciliIl;
+            button1.Text = "Kaydet";
+
+            guncellemeMi = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult sil = new DialogResult();
+            sil = MessageBox.Show("Silmek istiyormusunuz ?", "Uyarı", MessageBoxButtons.YesNo);
+
+            if (sil == DialogResult.Yes)
+            {
+                iller.Remove(listBox1.SelectedItem);
+                listBox1.DataSource = null;
+                listBox1.DataSource = iller;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = button1;
+        }
+    }
+}
---
+++ Array_List/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array_List
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // ARRAYLİST KULLANIMI
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            ArrayList arrayList1 = new ArrayList { }; // içindeki verileri object türünde tutar.
+
+            //arrayList1.add(50);
+
+            //arrayList1.add("Bilge Adam");
+
+            //arrayList1.Add(true);
+
+            object ilkEleman = arrayList1[0];
+            string ilkElaman2 = arrayList1[0].ToString();
+            int ilkEleman3 = Convert.ToInt32(arrayList1[0]);
+            //Button ilkEleman4 = { Button }arrayList1[0]; // unboxing
+
+            arrayList1.Remove(50);
+            arrayList1.RemoveAt(0);
+
+            arrayList1.Insert(3, "Deger");
+
+            arrayList1.Sort(); //Sıralama yapmak için
+            arrayList1.Reverse(); //Ters Çevirmek için
+
+            //arrayList1.TrimSize(); //Dizinin kapasitesini içindeki eleman sayısına eşitler
+
+            int boyut1 = arrayList1.Capacity;
+
+            arrayList1.Contains("Bilge Adam"); //Dizide o eleman var mı yok mu
+
+            arrayList1.IndexOf("Bilge Adam");
+
+            arrayList1.LastIndexOf("Bilge Adam");
+        }
+    }
+}
